7ff582daa119e46b476f824dd89c0cd6fba8448f
jpate7 <jpatel7@cub.uca.edu>
2020-10-20 21:30:49 -0500

    post final edits

--- guiProject/guiproject/MainFrame.java
+++ guiProject/guiproject/MainFrame.java
@@ -19,20 +19,29 @@ public class MainFrame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JFrame main = new JFrame("Contract Tracer");
-		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		main.setSize(700, 500);
 		main.setMaximumSize(new Dimension(720,530));
 		main.setMinimumSize(new Dimension(700,440));
-		
 		MainPanel panel = new MainPanel();
-		
 		main.getContentPane().add(panel);
 		main.setVisible(true);
 		main.pack();
 		main.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent we) {
 				//outputs the saved and edited data to a text document;
-				panel.doClose();
+				//main.setDefaultCloseOperation(main.EXIT_ON_CLOSE);	//try to exit on red
+				main.setDefaultCloseOperation(main.EXIT_ON_CLOSE);
+				int result = JOptionPane.showConfirmDialog(null, "Do you want to save any changes made to the file?");
+				if(result == JOptionPane.YES_OPTION)
+				{
+					//save changes
+					panel.doClose();	//then close
+				}
+
+				else if(result == JOptionPane.CANCEL_OPTION)
+				{
+					main.setDefaultCloseOperation(main.DO_NOTHING_ON_CLOSE);	//if cancel, then don't close
+				}
 			}
 		});
 
